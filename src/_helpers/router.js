2150dcb8b63d1320ff2e7d412b486a5ef2a2f2f6
Völgyi Barnabás <volgyi.barnabas@gmail.com>
2019-02-24 17:33:31 +0100

    Delete LoginPage and make Register default

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import HomePage from '../home/HomePage'
-import LoginPage from '../login/LoginPage'
 import RegisterPage from '../register/RegisterPage'
 
 Vue.use(Router);
@@ -11,7 +10,6 @@ export const router = new Router({
   mode: 'history',
   routes: [
     { path: '/', component: HomePage },
-    { path: '/login', component: LoginPage },
     { path: '/register', component: RegisterPage },
 
     // otherwise redirect to home
@@ -21,12 +19,12 @@ export const router = new Router({
 
 router.beforeEach((to, from, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/login', '/register'];
+  const publicPages = ['/register'];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = localStorage.getItem('user');
 
   if (authRequired && !loggedIn) {
-    return next('/login');
+    return next('/register');
   }
 
   next();
